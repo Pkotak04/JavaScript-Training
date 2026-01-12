3a118c515d1dc562e4711c33dbc560f0a06748cc
Param <paramkotak@hotmail.com>
2026-01-12 13:19:36 -0600

    Working with strings

---
+++ sandbox.js
@@ -0,0 +1,42 @@
+let age = 25;
+let year = 2019;
+
+// log things to console
+// console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100;
+
+// console.log(points);
+
+var score = 75;
+console.log(score);
+
+// Strings
+console.log("Hello, world");
+
+let email = "pkotak@academic.rrc.ca";
+console.log(email);
+
+// String concatentaion
+let firstName = "Param ";
+let lastName = "Kotak";
+
+let fullName = firstName + lastName;
+console.log(fullName);
+
+// Getting characters
+console.log(fullName[0]);
+
+// String length
+console.log(fullName.length);
+
+// String methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+console.log(index);
