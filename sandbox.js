e9f6ec52a161494aa045bbbc276c09e1d98b6a23
Pkotak04 <paramkotak@hotmail.com>
2026-01-14 15:31:01 -0600

    #

--- sandbox.js
+++ sandbox.js
@@ -21,14 +21,26 @@
 //     console.log(value);
 // });
 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+// const logPerson = (person, index) => {
+    //     console.log(`${index} - hello ${person}`);
+    // }
+    
+    // people.forEach(logPerson);
+    
 
-const logPerson = (person, index) => {
-    console.log(`${index} - hello ${person}`);
-}
+const ul = document.querySelector('.people');
 
-people.forEach(logPerson);
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
+let html = ``;
+
+people.forEach(person => {
+    // create html template
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
 
 
 
