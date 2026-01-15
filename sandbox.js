d71aa0477943e3f58693ab33bf9c33a2d4055877
Pkotak04 <paramkotak@hotmail.com>
2026-01-15 09:47:29 -0600

    Worked with key value pairs, object notation, dot notation and typeof keyword.

--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,23 @@
-let age = 25;
-let year = 2019;
+// object literals
 
-// log things to console
-console.log(age, year);
+let user = {
+    name: 'param',
+    age: 30,
+    email: 'pkotak@academic.rrc.ca',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+};
 
-age = 30;
-console.log(age);
+console.log(user);
+console.log(user.name);
 
-const points = 100;
+//user.age = 35;
+console.log(user.age);
 
-// console.log(points);
+const key = 'location';
 
-var score = 75;
-console.log(score);
+console.log(user['location']);
+user['name'] = 'chun-li';
+console.log(user['name']);
 
-// Strings
-console.log("Hello, world");
-
-let email = "pkotak@academic.rrc.ca";
-console.log(email);
-
-// String concatentaion
-let firstName = "Param ";
-let lastName = "Kotak";
-
-let fullName = firstName + lastName;
-console.log(fullName);
-
-// Getting characters
-console.log(fullName[0]);
-
-// String length
-console.log(fullName.length);
-
-// String methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
-console.log(result, fullName);
-
-let index = email.indexOf("@");
-console.log(index);
-
-// common string methods
-// let email = "pkotak@academic.rrc.ca";
-
-// let result = email.lastIndexOf("c");
-
-// let result = email.slice(0, 5);
-
-// let result = email.substring(4, 10);
-
-// let result = email.replace("k", "m");
-// let result = email.replace("k", "w");
-
-// console.log(result);
+console.log(typeof user);
