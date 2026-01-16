52dd202e3228414a51a1b75e450e93002b6c5b51
Pkotak04 <paramkotak@hotmail.com>
2026-01-16 11:35:42 -0600

    Learned about creating functions inside an object.

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,55 @@
 // object literals
 
+// let user = {
+//     name: 'param',
+//     age: 30,
+//     email: 'pkotak@academic.rrc.ca',
+//     location: 'berlin',
+//     blogs: ['why mac & cheese rules', '10 things to make with marmite']
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// //user.age = 35;
+// console.log(user.age);
+
+// const key = 'location';
+
+// console.log(user['location']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
 let user = {
     name: 'param',
     age: 30,
     email: 'pkotak@academic.rrc.ca',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login() {
+        console.log('the user logged in');
+    },
+    logout() {
+        console.log('the user logged out');
+    },
+    logBlogs() {
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        })
+    }
 };
 
-console.log(user);
-console.log(user.name);
+user.logBlogs();
+console.log(this);
 
-//user.age = 35;
-console.log(user.age);
+// user.login();
+// user.logout();
 
-const key = 'location';
+// const namee = 'mario';
+// namee.toUpperCase();
 
-console.log(user['location']);
-user['name'] = 'chun-li';
-console.log(user['name']);
 
-console.log(typeof user);
