c421f82d5190ff7bfdaded4ede6dac13bb570f50
Pkotak04 <paramkotak@hotmail.com>
2026-01-16 11:48:08 -0600

    Learned how to use store objects inside arrays.

--- sandbox.js
+++ sandbox.js
@@ -22,12 +22,22 @@
 
 // console.log(typeof user);
 
+// const blogs = [
+//     { title: 'why mac & cheese rules', likes: 30 },
+//     { title: '10 things to make with marmite', likes: 50 }
+// ];
+
+//console.log(blogs);
+
 let user = {
     name: 'param',
     age: 30,
     email: 'pkotak@academic.rrc.ca',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    blogs: [
+        { title: 'why mac & cheese rules', likes: 30 },
+        { title: '10 things to make with marmite', likes: 50 }
+    ],
     login() {
         console.log('the user logged in');
     },
@@ -38,13 +48,13 @@ let user = {
         //console.log(this.blogs);
         console.log('this user has written the following blogs:');
         this.blogs.forEach(blog => {
-            console.log(blog);
-        })
+            console.log(blog.title, blog.likes);
+        });
     }
 };
 
 user.logBlogs();
-console.log(this);
+//console.log(this);
 
 // user.login();
 // user.logout();
