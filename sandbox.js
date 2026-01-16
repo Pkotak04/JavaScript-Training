8b043403a69f8e7ffa86db66cdafcfb0b582ad5e
Pkotak04 <paramkotak@hotmail.com>
2026-01-16 13:38:48 -0600

    Working with math object.

--- sandbox.js
+++ sandbox.js
@@ -1,65 +1,20 @@
-// object literals
+// Math object
 
-// let user = {
-//     name: 'param',
-//     age: 30,
-//     email: 'pkotak@academic.rrc.ca',
-//     location: 'berlin',
-//     blogs: ['why mac & cheese rules', '10 things to make with marmite']
-// };
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-// console.log(user);
-// console.log(user.name);
+const area = 7.7;
 
-// //user.age = 35;
-// console.log(user.age);
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
-// const key = 'location';
+// random numbers
 
-// console.log(user['location']);
-// user['name'] = 'chun-li';
-// console.log(user['name']);
+const random = Math.random();
 
-// console.log(typeof user);
-
-// const blogs = [
-//     { title: 'why mac & cheese rules', likes: 30 },
-//     { title: '10 things to make with marmite', likes: 50 }
-// ];
-
-//console.log(blogs);
-
-let user = {
-    name: 'param',
-    age: 30,
-    email: 'pkotak@academic.rrc.ca',
-    location: 'berlin',
-    blogs: [
-        { title: 'why mac & cheese rules', likes: 30 },
-        { title: '10 things to make with marmite', likes: 50 }
-    ],
-    login() {
-        console.log('the user logged in');
-    },
-    logout() {
-        console.log('the user logged out');
-    },
-    logBlogs() {
-        //console.log(this.blogs);
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        });
-    }
-};
-
-user.logBlogs();
-//console.log(this);
-
-// user.login();
-// user.logout();
-
-// const namee = 'mario';
-// namee.toUpperCase();
-
-
+console.log(random);
+console.log(Math.round(random * 100));
